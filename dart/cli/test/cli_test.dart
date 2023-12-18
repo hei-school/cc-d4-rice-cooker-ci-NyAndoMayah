8467374173892c72225f257d6f3ab53278d9f6f4
NyAndoMayah <hei.mayah.3@gmail.com>
2023-12-18 21:24:28 +0300

    chore: test and deploy dart

--- dart/cli/test/cli_test.dart
+++ dart/cli/test/cli_test.dart
@@ -1,8 +1,31 @@
-import 'package:cli/cli.dart';
 import 'package:test/test.dart';
+import 'dart:io';
+import 'dart:async';
+import '../bin/rice_cooker.dart' as main_file;
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+
+var log = [];
+main() {
+  test('override print', overridePrint(() {
+    main_file.displayMenu();
+    expect(log, [ '\nWelcome to the Rice Cooker Simulator!',
+                               '1. Add rice',
+                               '2. Cook rice',
+                               '3. Steam',
+                               '4. Keep warm',
+                               '5. Remove rice',
+                               '6. Quit'
+]);
+  }));
 }
+void Function() overridePrint(void testFn()) => () {
+  var spec = new ZoneSpecification(
+    print: (_, __, ___, String msg) {
+      log.add(msg);
+    }
+  );
+  return Zone.current.fork(specification: spec).run(testFn);
+};
+
+
+
