f1bacffd3949a04a7f220268b43107f831973ea3
NyAndoMayah <hei.mayah.3@gmail.com>
2023-12-18 11:29:28 +0300

    fix: add menu

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+export function displayMenu() {
+    console.log('\nWelcome to the Rice Cooker Simulator!');
+    console.log('1. Add rice');
+    console.log('2. Cook rice');
+    console.log('3. Steam rice');
+    console.log('4. Keep warm');
+    console.log('5. Remove rice');
+    console.log('6. Quit');
+}
